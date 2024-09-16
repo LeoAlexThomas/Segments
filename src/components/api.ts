db05158eea4e0289ebd8876229adb0654280e1c0
LeoAlexThomas <leoalex960@gmail.com>
2024-09-16 19:31:49 +0530

    removed unwanted code in api file

--- src/components/api.ts
+++ src/components/api.ts
@@ -1,24 +1,8 @@
 import { getApiUrl } from "@/env";
-import { parseCookies } from "nookies";
-import { userTokenCookieName } from "./utils";
 const axios = require("axios");
-
-export const getAccessToken = (options: any) => {
-  const cookies = parseCookies(options && options.context);
-  const token = cookies[userTokenCookieName];
-  return token;
-};
 
 const api = (route: string, options?: any, baseUrl?: string) => {
   const combinedOptions = Object.assign({}, options);
-
-  axios.interceptors.request.use((request: any) => {
-    const token = getAccessToken(options);
-    if (token && !request.headers["Authorization"]) {
-      request.headers["Authorization"] = `Bearer ${token}`;
-    }
-    return request;
-  });
 
   const apiBaseUrl = baseUrl ?? getApiUrl();
   return axios({
